b03c3c616468cfc784ff943141ae86d7c6e48d91
Nishi Akinobu <nishi8024@nec.com>
2022-05-18 19:18:24 +0900

    main

--- public/app.js
+++ public/app.js
@@ -4,6 +4,7 @@ var yourcolor = {};
 
 yourcolor.mon;  //誕生月
 yourcolor.day;  //誕生日
+yourcolor.data;
 
 yourcolor.appOnReady = function() {
   window.onhashchange = function() {
@@ -101,30 +102,42 @@ yourcolor.landingView = function() {
 
 yourcolor.birthView = function() {
   var view = yourcolor.template('birth-view');
+  var birthday = yourcolor.mon + "/" + yourcolor.day;
 
-  $.ajax({
-    beforeSend: function(xhr){
-      xhr.overrideMimeType('text/html;charset=Shift_JIS');
-    },
-    type: "GET",
-    url: "data.json",
-    dataType: "json",
-    async: false,
-    cache: false
-  }).then(
-    function(json) {
-      console.log('読み込み成功');
-      console.log(json);
-    },
-    function (jqXHR, textStatus, errorThrown) {
-      console.log("読み込みに失敗しました");
-      console.log("jqXHR : " + jqXHR.status);
-      console.log("textStatus : " + textStatus);
-      console.log("errorThrown : " + errorThrown.message);
-    }
-  );
+  if (!yourcolor.data) {
+    $.ajax({
+      beforeSend: function(xhr){
+        xhr.overrideMimeType('text/html;charset=Shift_JIS');
+      },
+      type: "GET",
+      url: "data.json",
+      dataType: "json",
+      async: false,
+      cache: false
+    }).then(
+      function(json) {
+        yourcolor.data = JSON.parse(JSON.stringify(json));
+      },
+      function (jqXHR, textStatus, errorThrown) {
+        console.log("JSONファイルの読み込み失敗");
+        console.log("jqXHR : " + jqXHR.status);
+        console.log("textStatus : " + textStatus);
+        console.log("errorThrown : " + errorThrown.message);
+      }
+    );  
+  }
 
-  $('body').css('background-color','#5383c3');
+  var item = yourcolor.data.find((v) => v.Birthday === birthday)
+  var r = parseInt(item.ColorCode[1]+item.ColorCode[2],16);
+  var g = parseInt(item.ColorCode[3]+item.ColorCode[4],16);
+  var b = parseInt(item.ColorCode[5]+item.ColorCode[6],16);
+
+  view.find('#color-name').text(item.ColorName);
+  $('body').css('background-color',item.ColorCode);
+  if ((r+g+b) < 512) {
+    view.find('#color-name').css('color', 'white');
+  }
+ 
   $('body').fadeIn(500);
   return view;
 }
